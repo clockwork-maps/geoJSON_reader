9388ae487f104dbe94a47ed3d7cf028a37c5754d
clockwork-maps <131809002+clockwork-maps@users.noreply.github.com>
2023-12-14 14:48:15 +0100

    Added more panels to the info section. Polished up table formatting a little.

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -1,9 +1,16 @@
 import * as d3 from 'd3';
+import { useState } from 'react';
 import type { Geometry, GeoJsonProperties, Feature } from 'geojson'
 
+type dtVal = "metadata" | "props" | "propTypes"
 
 interface metadataTypes{
     [index: string]: string | number,
+}
+
+interface DTableProps {
+    target: metadataTypes | GeoJsonProperties | undefined,
+    parser: dtVal | undefined
 }
 
 interface rawExpected{
@@ -18,69 +25,63 @@ interface DTProps{
 }
 
 export default function DataTable(props: DTProps){
+    const [panelTarget, setPanelTarget] = useState<metadataTypes | GeoJsonProperties | undefined>();
+    const [panelType, setPanelType] = useState<dtVal | undefined>();
+    const [panelFlag, setPanelFlag] = useState<"Meta" | "Finfo" | "Ginfo" | "Props" | "undefined">("undefined")
     const data: rawExpected = props.data;
-    // console.log(data);
     const metadata: metadataTypes | undefined = data != undefined ? data.metadata : undefined;
-    // console.log(metadata);
     const features: Array<Feature<Geometry, GeoJsonProperties>> | undefined = data.features;
     const fKeys: string[] | undefined = Object.keys(features![0].properties as object);
+    const finfo: {[index:string]: string | number | undefined} = {
+        "length" : features!.length,
+        "fkeys" : (Object.keys(features![0] as object) as Array<string>).join(', ')
+    }
     const geometries : Geometry[] | undefined = data.geometries;
     const headerBools: boolean[] = [metadata != undefined, features != undefined, geometries != undefined,  fKeys != undefined];
-    const headers: string[] = ["Meta", "Finfo", "Ginfo", "Props"];
-    function MTable(){
-        const mkeys: string[] | undefined = Object.keys(metadata as object);
-        const dID: string = `dialog${Math.round(Math.random() * 15)}`;
-        return(
+    const headers: ("Meta" | "Finfo" | "Ginfo" | "Props")[] = ["Meta", "Finfo", "Ginfo", "Props"];
+    const tableObjects: {[index: string]: metadataTypes | GeoJsonProperties | {[index:string]: string | number | undefined} | Geometry[] | undefined} = {
+        "Meta" : metadata,
+        "Finfo" : finfo,
+        "Ginfo": geometries,
+        "Props" : features![0].properties
+    }
+    const tableTypes: {[index:string]: dtVal} = {
+        "Meta" : "metadata",
+        "Props" : "props"
+    }
+    function DTable(props: DTableProps){
+        const tkeys: string[] | undefined = props.target !== undefined ? Object.keys(props.target as object) : undefined;
+        return props.target !== undefined ? (
             <table className="diTable">
                 <tbody>
-                    {mkeys.map(key=>{
+                    {tkeys!.map(key=>{
                         return(
-                            <tr key={`${key}${Math.round(Math.random()*10)}`} onMouseEnter={(e)=>{
-                                let target: EventTarget = e.target;
-                                let dialog: HTMLElement | null = document.getElementById(dID);
-                                (target as HTMLElement)!.appendChild(dialog!);
-                                (dialog as HTMLDialogElement).show();
-                                dialog!.innerHTML = `<p class="infoText"> <span class="itHeader">${key}:</span> ${metadata![key]}</p>`
-                            }} onMouseLeave={()=>{
-                                let dialog: HTMLElement | null = document.getElementById(dID);
-                                (dialog as HTMLDialogElement).close();
-                            }}>
-                                <th onMouseEnter={(e)=>{
-                                    let target: HTMLElement | null = (e.target as HTMLElement).parentElement;
-                                    let dialog: HTMLElement | null = document.getElementById(dID);
-                                    target!.appendChild(dialog!);
-                                    (dialog as HTMLDialogElement).show();
-                                    dialog!.innerHTML = `<p class="infoText"> <span class="itHeader">${key}:</span> ${metadata![key]}</p>`
-                                }} onMouseLeave={()=>{
-                                    let dialog: HTMLElement | null = document.getElementById(dID);
-                                    (dialog as HTMLDialogElement).close();
-                                }}>{key}</th>
-                                <td onMouseEnter={(e)=>{
-                                    let target: HTMLElement | null = (e.target as HTMLElement).parentElement;
-                                    let dialog: HTMLElement | null = document.getElementById(dID);
-                                    target!.appendChild(dialog!);
-                                    (dialog as HTMLDialogElement).show();
-                                    dialog!.innerHTML = `<p class="infoText"> <span class="itHeader">${key}:</span> ${metadata![key]}</p>`
-                                }} onMouseLeave={()=>{
-                                    let dialog: HTMLElement | null = document.getElementById(dID);
-                                    (dialog as HTMLDialogElement).close();
-                                }}>{metadata![key]}</td>
+                            <tr key={`${key}${Math.round(Math.random()*10)}`} >
+                                <th>{key}</th>
+                                <td>{props.parser !== "props" ? props.target![key] : typeof props.target![key]}</td>
                             </tr>
                         )
                     })}
                 </tbody>
-                <dialog className='infoPopup' id={dID} />
             </table>
+        ) : (
+            <>
+                <span className="promptText">Select a tab to draw a table</span>
+            </>
         )
     }
     return (
         <>
             <article className="dataTable">
-                <section className="spanRow">{headerBools.map((bool, i) => {
-                    return (bool ? <span className='srHeader' key={`${headers[i]}${Math.round(Math.random()*10)}`}>{headers[i]}</span> : null)
+                <section className="spanRow" data-flag={panelFlag}>{headerBools.map((bool, i) => {
+                    return (bool ? <span className={`srHeader sr${headers[i]}`} key={`${headers[i]}${Math.round(Math.random()*10)}`} onClick={()=>{
+                        setPanelTarget(tableObjects[headers[i]]);
+                        setPanelType(tableTypes[headers[i]]);
+                        setPanelFlag(headers[i])
+                    }} >{headers[i]}</span> : null)
                 })}</section>
                 <section className="dataInfo">
-                    <MTable />
+                    <DTable target={panelTarget} parser={panelType}/>
                 </section>
             </article>
         </>
